391295aff70e10229e31aa91acddb0f099b10ae4
Eduardo Dallabella Lima <eduardodallabella@gmail.com>
2024-02-04 14:30:01 -0300

    new stuff

---
+++ %/modulos.py
@@ -0,0 +1,13 @@
+print(27 / 8, "<-- Este é o resultado de 27 ÷ 8")
+#    3.375   <-- Este é o resultado de 27 ÷ 8
+print("----------")
+# ----------
+print(27 % 8, "<-- Este é o resultado de 27 % 8")
+print("mas como o resultado é 3?")
+print("Então, para começar, você deve fazer o produto de 8 e 3 (que é o resultado de 27 ÷ 8)")
+# 8  | 27.000  <-- Então, nesta parte você faz o produto de 8 e 3 (que é o resultado de 27 ÷ 8)
+print("Em seguida, com 24 (que é o resultado do produto), você deve "
+      "subtrair o dividendo pelo resultado do produto de 8 e 3")
+#    -24       <-- then you subtract the dividend by the rusult of the product of 8 and 3
+# ----------
+#       3    <-- Remainder
---
+++ functions/message.py
@@ -0,0 +1,7 @@
+def message(what, number): # gives statements("what" and "number") to enter something(can be a string, bollean, etc
+    print("Entrar", what, "número", number)
+
+
+message("telefone", 11)
+message("preço", 5),
+message("número", "número")
